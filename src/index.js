ec1b873e5e32f3183a20dc885cf380e47a73ef2a
mohammad-atique <92262714+mohammad-atique@users.noreply.github.com>
2022-03-30 17:14:20 +0530

    added cors package

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const goodKartsController = require("./controllers/goodKart.controllers.js")
-
+const cors = require('cors')
 const app = express();
-
+app.use(cors())
 app.use(express.json());
 
 
