8fbf9a5b318e9d95112add1eaa36e253d92043a9
minakshikalambe <97457265+minakshikalambe@users.noreply.github.com>
2022-03-31 12:39:59 +0530

    Update product.models.js

--- src/models/product.models.js
+++ src/models/product.models.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    url: { type: String, required: true },
+    image: { type: String, required: true },
+    name: { type: String, required: true },
+    des: { type: String, required: true },
     price: { type: Number, required: true},
-    strikeprice:{ type: Number, required: true},
+    strprice:{ type: Number, required: true},
     discount:{ type: Number, required: false},
   }
 );
