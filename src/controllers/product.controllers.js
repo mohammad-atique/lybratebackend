6ad47037ae33f7a1fc1d230c9da243d8ca179faa
minakshikalambe <97457265+minakshikalambe@users.noreply.github.com>
2022-03-31 13:44:55 +0530

    Update product.controllers.js

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -27,7 +27,7 @@ router.get("",async (req,res)=>{
 
 router.get("/:id",async (req,res)=>{
   try {
-      const product=await Product.findById().lean().exec();
+      const product=await Product.findById().lean(req.params.id).exec();
       
       return res.status(200).send(product)
   } catch (error) {
