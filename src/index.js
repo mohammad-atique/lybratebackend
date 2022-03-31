6261552fdb70e5450c38e1a53337db1e8872d4c4
Soumya <a2soumya.ranjan@gmail.com>
2022-03-31 11:48:50 +0530

    added login authentication

--- src/index.js
+++ src/index.js
@@ -12,4 +12,34 @@ app.use("/goodKarts", goodKartsController);
 const productController = require("./controllers/product.controllers");
 app.use("/vitaminandsupplementproducts", productController); 
 
+//Soumya
+
+const userController=require("./controllers/user-controller")
+app.use("/users",userController)
+const {register,login,newtoken}=require("./controllers/auth-controller")
+
+const passport = require("./configs/google-oauth")
+
+app.post("/register",register)
+app.post("/login",login)
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+  	[ 'email', 'profile' ] }
+));
+ 
+app.get( '/auth/google/callback',
+    passport.authenticate( 'google', {
+         
+        failureRedirect: '/auth/google/failure', session:false
+}),
+    function (req, res){
+        const token = newtoken(req.user)
+        return res.status(200).send({user:req.user, token})
+    }
+
+);
+
+
+
 module.exports = app;
