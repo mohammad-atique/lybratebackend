5ad03e47e60d940b861f28e32a30f0d53b70f602
mohammad-atique <92262714+mohammad-atique@users.noreply.github.com>
2022-04-01 21:05:16 +0530

    test

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -3,7 +3,7 @@ const express= require("express");
 const User = require("../models/user-model.js")
 const router = express.Router();
 router.get("/:email",async (req,res)=>{
-    const user=await User.findOne({title: req.params.email).lean().exec();
+    const user=await User.findOne({title: req.params.email}).lean().exec();
 
     return res.status(200).send(user);
 })
