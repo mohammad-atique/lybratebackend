7d93528c20c510a49599944a1e7ef3d1fba65940
pankajnandi <pkandpal28@gmail.com>
2022-04-02 09:59:10 +0530

    add

---
+++ src/controllers/mail.controllers.js
@@ -0,0 +1,37 @@
+
+const express = require("express");
+
+const transporter = require("../configs/mail");
+
+const user = {
+  email: "pk@pk",
+  name: "pankaj",
+};
+
+const router = express.Router();
+router.post("/", async function (req, res) {
+  const otp = Math.floor(Math.random() * (999999 - 100000) + 100000);
+console.log(otp)
+  try {
+    
+    transporter.sendMail({
+      from: '"lybrate admin" <admin@lybrate.com>', // sender address
+      to: user.email, // list of receivers
+      subject: "verify OTP - checkout process", // Subject line
+      text: `Hello ${user.name},
+      Please verify your OTP. Your OTP is below:
+      ${otp}`, // plain text body
+      html: `<b>Hello ${user.name},
+        <br>
+        Please verify your OTP. Your OTP is below:
+        <br>
+        ${otp}</b>`, // html body
+    });
+
+    return res.status(201).send({ otp:otp});
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
+
+module.exports = router;
